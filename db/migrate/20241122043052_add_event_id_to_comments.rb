4ddd9d64dc3b8abd8bd461f521a8e87b1b325a4b
Dan Le <dantle@umail.ucsb.edu>
2024-11-26 21:19:49 -0800

    removed duplicate event

--- db/migrate/20241122043052_add_event_id_to_comments.rb
+++ db/migrate/20241122043052_add_event_id_to_comments.rb
@@ -1,5 +1,9 @@
 class AddEventIdToComments < ActiveRecord::Migration[8.0]
-  def change
-    add_column :comments, :event_id, :integer
+  class AddEventIdToComments < ActiveRecord::Migration[8.0]
+    def change
+      unless column_exists?(:comments, :event_id)
+        add_column :comments, :event_id, :integer
+      end
+    end
   end
 end
