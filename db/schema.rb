cc575cfef66cbec6ef7556735bdb1cb35b6dc75d
huangderful <richard_huang@ucsb.edu>
2024-11-26 16:31:16 -0800

    authentication and forced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[8.0].define(version: 2024_11_22_075140) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_26_225634) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
@@ -55,8 +55,8 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_22_075140) do
   end
 
   create_table "saved_events", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "event_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "event_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_saved_events_on_event_id"
@@ -67,6 +67,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_22_075140) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "posts"
