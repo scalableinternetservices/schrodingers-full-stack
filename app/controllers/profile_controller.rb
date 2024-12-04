76231e54dd0a025cd869854d06498e6a141d9617
Ishan Jain <ishanjain42@gmail.com>
2024-12-04 10:18:38 -0800

    better UI

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -5,6 +5,6 @@ class ProfileController < ApplicationController
       redirect_to '/'
     end
     @user = User.find_by(username: session[:current_user_id])
-    @events = @user.events  # Only the events saved by the current user
+    @events = @user&.events  # Only the events saved by the current user
   end
 end
