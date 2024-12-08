17a808e8d7cfb3c5020315f3020678e8eff0e045
huangderful <richard_huang@ucsb.edu>
2024-12-07 20:29:33 -0800

    all tsung logs for all but one vs pagination

--- tsung_logs/gen_graphs.sh
+++ tsung_logs/gen_graphs.sh
@@ -8,10 +8,11 @@ for GRAPH_DIR in "${DIRS[@]}"; do
     # Define the paths for DIR1 and DIR2
     DIR1_OPTIMIZATION="Pagination"
     DIR1="./pagination/tsung_$GRAPH_DIR/tsung.log"
+    echo $DIR1
 
-
-    DIR2_OPTIMIZATION="Baseline"
-    DIR2="./preoptimizations/tsung_${GRAPH_DIR}_pre_optimization/tsung.log"
+    DIR2_OPTIMIZATION="Pagination+Indexing+Caching"
+    DIR2="./all_but_one/tsung_$GRAPH_DIR/tsung.log"
+    echo $DIR2
     # Run the tsplot command with the current GRAPH_DIR
-    python2 /usr/bin/tsplot -d "graphs/pagination/$GRAPH_DIR" $DIR1_OPTIMIZATION $DIR1 $DIR2_OPTIMIZATION $DIR2
+    python2 /usr/bin/tsplot -d "graphs/all_but_one/$GRAPH_DIR" $DIR1_OPTIMIZATION $DIR1 $DIR2_OPTIMIZATION $DIR2
 done
